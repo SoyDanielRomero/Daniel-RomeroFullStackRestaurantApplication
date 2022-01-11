15282cd84ca0f152d62d786b4512a4fc4e290e31
Daniel Romero <daniromerro@gmail.com>
2022-01-10 23:55:05 -0500

    Docker

--- lib/awsec2docker_express-stack.ts
+++ lib/awsec2docker_express-stack.ts
@@ -20,7 +20,7 @@ export class Awsec2dockerExpressStack extends cdk.Stack {
  const helloTaskDefinition = new ecs.Ec2TaskDefinition(this, 'hello-task-definition', {});
 
   const helloContainer = helloTaskDefinition.addContainer('hello', {
-    image: ecs.ContainerImage.fromDockerHub ('soydanielromero/capstone:frontendclean'),
+    image: ecs.ContainerImage.fromDockerHub ('soydanielromero/capstone:frontendtry'),
     memoryLimitMiB: 128
   });
 
